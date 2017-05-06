723c0a314155464ecc4b2451fd6379459ef3181f
Eliteall <eliteall.project@gmail.com>
2017-05-06 23:50:59 +0900

    Hello! Now Loading

---
+++ angular4-not-spa/gulpfile.babel.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const tsc = require('gulp-typescript');
+const tsProject = tsc.createProject('./tsconfig.json');
+const sourcemaps = require('gulp-sourcemaps');
+const webpackStream = require("webpack-stream");
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config.babel");
+const browserSync = require('browser-sync');
+
+gulp.task("webpack", () => {
+    return webpackStream(webpackConfig, webpack)
+        .pipe(gulp.dest('public'));
+});
+
+
+gulp.task('serve', ['webpack'], () => {
+    return browserSync.init(null, {
+        logLevel: "debug",
+        open: true,
+        port: 3000,
+        online: false,
+        ui: false,
+        server: {
+            baseDir: 'public/'
+        },
+        reloadDelay: 1000
+    })
+});
+
+gulp.task('default', ['serve']);
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 import '../css/styles.scss'
+platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.production) {
     enableProdMode();
